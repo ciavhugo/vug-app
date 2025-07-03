2fa708adca0236e8adc0a12ef11239ad6fdb1368
ciavhugo <ciavhm@gmail.com>
2025-07-03 20:50:54 -0300

    fix: refactor msg

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -2,7 +2,7 @@ export default function Blog() {
   return (
     <section className="w-full max-w-[700px] mx-auto min-h-screen flex items-center justify-center">
       <p className="text-center text-lg">
-        Esta página está sendo desenvolvida...
+        "⚠️ Página em Desenvolvimento..."
       </p>
     </section>
   );
