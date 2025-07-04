0c0b13094d65184c67f0ab63a312d684eef10daf
ciavhugo <ciavhm@gmail.com>
2025-07-04 17:24:40 -0300

    fix: blog

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,7 +1,7 @@
 export default function Blog() {
   return (
     <section className="w-full max-w-[700px] mx-auto min-h-screen flex items-center justify-center">
-      <p className="text-center text-lg">
+      <p className="text-center text-[14px]">
         "⚠️ Página em Desenvolvimento..."
       </p>
     </section>
