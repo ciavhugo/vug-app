ba1c2f5ced23ba651a22e2b31c165b60043b4dce
ciavhugo <ciavhm@gmail.com>
2025-09-25 18:48:21 -0300

    feat: add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { HeaderNavigationMenu } from "@/components/frames/HeaderNavigationMenu";
 
@@ -25,36 +26,52 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-TFKV7KDCV0"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TFKV7KDCV0');
+          `}
+        </Script>
+      </head>
       <body
-        className={`${fontInter.variable} ${robotoMono.variable} antialiased select-none  [user-select:none] [webkit-user-drag:none]`}
+        className={`${fontInter.variable} ${robotoMono.variable} antialiased select-none [user-select:none] [webkit-user-drag:none]`}
       >
         {/* Fundo fixo + gradiente */}
         <div className="fixed top-0 left-0 w-screen h-[40%] z-[-1] pointer-events-none max-[429px]:hidden">
           <div
             className="
-      w-full h-full 
-      bg-[url('/backgroundImg.png')] 
-      bg-no-repeat 
-      bg-cover 
-      bg-center
-      max-[768px]:bg-[position:center_top_30%]
-      max-[600px]:bg-[position:center_top_50%]
-      max-[480px]:bg-[position:center_top_65%]
-      opacity-10
-    "
+              w-full h-full 
+              bg-[url('/backgroundImg.png')] 
+              bg-no-repeat 
+              bg-cover 
+              bg-center
+              max-[768px]:bg-[position:center_top_30%]
+              max-[600px]:bg-[position:center_top_50%]
+              max-[480px]:bg-[position:center_top_65%]
+              opacity-10
+            "
           />
           <div className="absolute bottom-0 left-0 w-full h-30 bg-gradient-to-b from-[#121212]/0 to-[#121212]" />
         </div>
-        
-          <div className="fixed top-0 left-0 w-full z-10 backdrop-blur-md flex justify-center px-4">
-            <HeaderNavigationMenu />
-          </div>
 
-          <div className="relative flex flex-col items-center min-h-screen mx-6 sm:mx-4">
-            {children}
-          </div>   
+        <div className="fixed top-0 left-0 w-full z-10 backdrop-blur-md flex justify-center px-4">
+          <HeaderNavigationMenu />
+        </div>
 
-        {/* <div className="h-6 opacity-0 pointer-events-none" /> */}
+        <div className="relative flex flex-col items-center min-h-screen mx-6 sm:mx-4">
+          {children}
+        </div>
 
         <footer className="w-full text-center py-2 text-sm max-[600px]:text-[12px] text-gray-600">
           © {new Date().getFullYear()} —{" "}
