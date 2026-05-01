1b243901ae95272b270d9db2a026304e4f057d65
ciavhugo <ciavhm@gmail.com>
2026-05-01 13:23:29 -0300

    feat: Logo

--- src/components/frames/HeaderNavigationMenu/index.tsx
+++ src/components/frames/HeaderNavigationMenu/index.tsx
@@ -15,7 +15,7 @@ export function HeaderNavigationMenu() {
 
   return (
     <section className="relative mt-4 flex h-[60px] w-full max-w-[805px] items-center justify-between overflow-hidden rounded-2xl border border-white/10 bg-white/[0.035] px-4 backdrop-blur-md">
-      <Link
+      {/* <Link
         href="/"
         aria-label="Ir para a página inicial"
         className="relative hidden h-12 w-12 shrink-0 items-center justify-center sm:flex"
@@ -28,6 +28,28 @@ export function HeaderNavigationMenu() {
           className="h-12 w-12"
           priority
         />
+      </Link> */}
+
+      <Link
+        href="/"
+        aria-label="Go to homepage"
+        className="flex items-center gap-2 group cursor-pointer"
+      >
+        {/* Logo com glow */}
+        <div className="relative">
+          <div className="absolute inset-0 bg-black dark:bg-white border-8 blur-lg opacity-0 group-hover:opacity-10 transition duration-500"></div>
+
+          <div className="h-8 w-8 bg-zinc-100 dark:bg-white/10 border border-zinc-200 dark:border-white/20 flex items-center justify-center transition-all duration-700 ease-in-out">
+            <span className="text-sm font-mono font-bold text-zinc-900 dark:text-white">
+              VM
+            </span>
+          </div>
+        </div>
+
+        {/* Nome */}
+        <span className="font-bold tracking-widest text-zinc-900 dark:text-white uppercase text-sm hidden sm:block">
+          NAMEVUG
+        </span>
       </Link>
 
       <nav className="relative flex flex-1 items-center justify-center gap-2 text-[13px] font-normal uppercase max-[550px]:text-[12px] sm:ml-auto sm:flex-none sm:justify-end sm:gap-3">
